106a287c776fe43ebdca2edcd3c16eb9cc96955f
Soda <oluwaseunajb@gmail.com>
2025-01-21 23:33:25 +0100

    add orders management functionality with new orders page and styles

--- shop/scripts/payments.js
+++ shop/scripts/payments.js
@@ -1,6 +1,9 @@
+import { cart } from "../../data/cart.js";
+import { getOrdersFromCart, orders } from "../../data/orders.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
 import { formatCurrency } from "./utils/moneyUtil.js";
 import { renderNavBarandFooter } from "./utils/rendering.js";
+import { getFromStorage, saveToStorage } from "./utils/storage.js";
 
 
 renderNavBarandFooter();
@@ -10,6 +13,8 @@ const amount = JSON.parse(localStorage.getItem('total'));
 
 document.querySelector(".total-amount").innerHTML = `₦ ${formatCurrency(amount)}`;
 
+export let ref;
+
 export function payWithPaystack() {
     const email = document.getElementById('email').value;
 
@@ -17,22 +22,31 @@ export function payWithPaystack() {
         alert("Kindly enter Payer's email");
         return;
     }
-    console.log('got here')
 
     const handler = PaystackPop.setup({
-        key: 'pk_test_8bbbaf3344e247196567482948a2d26f6b1d9464', 
+        key: 'pk_test_8bbbaf3344e247196567482948a2d26f6b1d9464',
         email: email,
         amount: amount,
-        currency: 'NGN', 
+        currency: 'NGN',
         callback: function (response) {
-            alert('Payment complete! Reference: ' + response.reference);
+            const promise = new Promise((resolve) => {
+                const ref = response.reference;
+                saveToStorage("ref", ref);
+                resolve("orders.html")
+            });
+    
+            promise.then((url) => {
+                window.location.href = url;
+                orders = cart;
+                cart = [];
+                saveToStorage("orders", orders)
+                saveToStorage("cart", cart)
+            });
         },
         onClose: function () {
-            alert('Payment window closed.');
+            alert("You will be redirected soon!");
         },
     });
 
     handler.openIframe();
 }
-
-
